52919301dc53eb08fd7c912b4bede05b06d2f42f
zerle <1163494244@qq.com>
2020-07-18 13:19:59 +0800

    webpack基础配置 Signed-off-by: zerle <1163494244@qq.com>

---
+++ webpack.config.js
@@ -0,0 +1,83 @@
+// 首先引入插件
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const isDev = process.env.NODE_ENV === 'development';
+const config = require('./public/config')[isDev ? 'dev' : 'build'];
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),  // 必须是绝对路径
+        filename: 'bundle.[hash:6].js',
+        publicPath: '/'  //通常是CDN地址
+    },
+    mode: isDev ? 'development' : 'production',
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                use: ['babel-loader'],
+                exclude: /node_modules/ //排除 node_modules 目录
+            },
+            {
+                test: /\.(le|c)ss$/,
+                use: ['style-loader', 'css-loader', {
+                    loader: 'postcss-loader',
+                    options: {
+                        plugins: () => {
+                            return [
+                                require('autoprefixer')({
+                                    "overrideBrowserslist": [
+                                        ">0.25%",
+                                        "not dead"
+                                    ]
+                                })
+                            ]
+                        }
+                    }
+                }, 'less-loader'],
+                exclude: /node_modules/
+            },
+            {
+                test: /\.(png|jpg|gif|jpeg|webp|svg|eot|ttf|woff|woff2)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 10240, //10K
+                            esModule: false,
+                            name: '[name]_[hash:6].[ext]'
+                        }
+                    }
+                ],
+                exclude: /node_modules/
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            filename: 'index.html', //打包后的文件名
+            minify: {
+                removeAttributeQuotes: false,  //是否删除属性的双引号
+                collapseWhitespace: false //是否折叠空白
+            },
+            config: config.template
+            // hash: true  //是否加上hash，默认是 false
+        }),
+        new CleanWebpackPlugin({
+            cleanOnceBeforeBuildPatterns:['**/*', '!dll', '!dll/**'] //不删除dll目录下的文件
+        })
+    ],
+    devServer: {
+        port: '3000', //默认是8080
+        quiet: false, //默认不启用
+        inline: true, //默认开启 inline 模式，如果设置为false,开启 iframe 模式
+        stats: "errors-only",  //终端仅打印 error
+        overlay: false, //默认不启用
+        clientLogLevel: 'silent', //日志等级
+        compress: true  //是否启用 gzip 压缩
+    },
+    devtool: 'cheap-module-eval-source-map'  // 开发环境下使用
+}
